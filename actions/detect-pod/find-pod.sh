ecc629b972e377f48b7302d3cecbe10026949159
Octane CI <octane@phase2technology.com>
2021-12-10 09:23:06 -0500

    Move find-pod script from octane into action

---
+++ actions/detect-pod/find-pod.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# Determine if a pod for a release is running
+# Usage: find-pod.sh release-name
+# Returns: "True" if pod is running
+
+releaseName="$1"
+namespace=${KUBE_NAMESPACE:-${PROJECT_NAME}}
+
+if [ -z "$releaseName" ]; then
+  currentBranch="$(git symbolic-ref HEAD 2>/dev/null)"
+  currentBranch=${currentBranch##refs/heads/}
+  releaseName="${PROJECT_NAME}-${currentBranch}"
+fi
+
+# Determine the name of the webcontainer pod for this namespace.
+podName=$(kubectl get pods -n ${namespace} -l release=$releaseName,webcontainer=true -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}')
+echo "$podName"
