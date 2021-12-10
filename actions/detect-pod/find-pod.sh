4f9a08d3221604c5f1605acf579111acb6fbc083
Octane CI <octane@phase2technology.com>
2021-12-10 14:07:13 -0500

    Fix generator references

--- actions/detect-pod/find-pod.sh
+++ actions/detect-pod/find-pod.sh
@@ -11,12 +11,12 @@ source $(dirname $0)/.functions.sh
 if [[ -e ".env" ]]; then
   export $(grep -v '^#' ".env" | xargs)
 fi
-export PROJECT_NAME=${PROJECT_NAME:-GENERATOR_PROJECT_NAME}
+export PROJECT_NAME=${PROJECT_NAME:-$GENERATOR_PROJECT_NAME}
 
 releaseName=$(release_name $1)
 
 # Determine the name of the webcontainer pod for this namespace.
-namespace=${KUBE_NAMESPACE:-GENERATOR_NAMESPACE}
+namespace=${KUBE_NAMESPACE:-$GENERATOR_NAMESPACE}
 namespace=${namespace:-${projectName}}
 podName=$(kubectl get pods -n ${namespace} -l release=$releaseName,webcontainer=true -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}')
 echo -n "$podName" | tr '[:upper:]' '[:lower:]'
