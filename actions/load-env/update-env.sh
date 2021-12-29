139a668f73278737abfec3e228ca1ce62381a300
Octane CI <octane@phase2technology.com>
2021-12-29 10:13:38 -0500

    Remove debugging

--- actions/load-env/update-env.sh
+++ actions/load-env/update-env.sh
@@ -6,23 +6,24 @@ if [[ -e ".env" ]]; then
   export $(grep -v '^#' ".env" | xargs)
 fi
 export PROJECT_NAME=${PROJECT_NAME:-$GENERATOR_PROJECT_NAME}
-export PROJECT_ROOT=${PROJECT_ROOT:-$(pwd)}
 
 export CI_REGISTRY=ghcr.io
 export CI_URL="devcloud.fayze2.com"
 export WEB_IMAGE=${CI_REGISTRY}/${GITHUB_REPO}/${PROJECT_NAME}
 export CI_BRANCH=${GITHUB_REF##refs/heads/}
 
+rootDir=$(pwd)
 # Override config/cache paths for various tools
-export HELM_CONFIG_HOME=${PROJECT_ROOT}/.config/helm/config
-export HELM_CACHE_HOME=${PROJECT_ROOT}/.config/helm/cache
-export HELM_DATA_HOME=${PROJECT_ROOT}/.config/helm/data
+export HELM_CONFIG_HOME=${rootDir}/.config/helm/config
+export HELM_CACHE_HOME=${rootDir}/.config/helm/cache
+export HELM_DATA_HOME=${rootDir}/.config/helm/data
 mkdir -p ${HELM_CONFIG_HOME}
 mkdir -p ${HELM_CACHE_HOME}
 mkdir -p ${HELM_DATA_HOME}
-export YARN_CACHE_FOLDER=${PROJECT_ROOT}/.config/yarn/cache
+export YARN_CACHE_FOLDER=${rootDir}/.config/yarn/cache
 mkdir -p ${YARN_CACHE_FOLDER}
 
+# Export these variables into the GITHUB_ENV file
 varList=(
   "CI_REGISTRY"
   "CI_URL"
