28760369f023bbad477d07c851101106472324cc
Mike Potter <mpotter@phase2technology.com>
2022-02-01 10:38:20 -0500

    Add reset-workspace-owner action

---
+++ actions/reset-workspace-owner/entrypoint.sh
@@ -0,0 +1,27 @@
+#!/bin/sh -l
+
+SCRIPT=$(readlink -f "$0")
+SCRIPT_PATH=$(dirname "$SCRIPT")
+
+export USER_UID=$1
+
+echo "Updating ownership permissions on workspace directory ${2}"
+echo "  user: $USER_UID"
+
+# The current working directory in the container is the workspace we need to check and modify
+ls -la .
+
+echo
+echo "Looking for files/directories in current working directory not owned by \"$USER_UID\""
+echo
+find . ! -user $USER_UID -exec $SCRIPT_PATH/update_file_object.sh "{}" "$USER_UID" \;
+
+echo "Looking for files/directories in ${HOME} directory not owned by \"$USER_UID\""
+# Also check the $HOME folder
+if [ -e "$HOME" ]; then
+  cd $HOME
+  ls -la .
+  echo
+  echo
+  find . ! -user $USER_UID -exec $SCRIPT_PATH/update_file_object.sh "{}" "$USER_UID" \;
+fi
